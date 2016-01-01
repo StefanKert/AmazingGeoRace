e22c4a2fc9e7560aa43147e7e99d302f7a6248e6
Stefan Kert <stefan.kert@inode.at>
2016-01-01 12:56:58 +0100

    Added files

---
+++ AmazingGeoRace/AmazingGeoRace/Models/RouteRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+using AmazingGeoRace.Domain;
+
+namespace AmazingGeoRace.Models {
+
+    [DataContract]
+	public class RouteRequest : Request {
+		[DataMember]
+		public Guid RouteId { get; private set; }
+
+	    public RouteRequest(Credentials credentials, Guid routeId): base(credentials) {
+	        RouteId = routeId;
+	    }
+	}
+}
---
+++ AmazingGeoRace/AmazingGeoRace/Views/SolutionDialog.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace AmazingGeoRace.Views
+{
+    public sealed partial class SolutionDialog : ContentDialog
+    {
+        public string Solution => TbSolution.Text;
+
+        public SolutionDialog() {
+            this.InitializeComponent();
+        }
+    }
+}
